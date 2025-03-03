cc448b174a992ad0758379ffe314416ba70e86e0
Sesu <hermonamanuel246@gmail.com>
2025-03-02 21:10:39 -0500

    update on question5

--- question5.c
+++ question5.c
@@ -46,7 +46,9 @@ int my_barrier_destroy(my_barrier_t *barrier) {
 my_barrier_t barrier;
 int total_grade = 0;
 int grades[10];
+int total_bellcurve_grade = 0;
 pthread_mutex_t lock;
+pthread_mutex_t file_lock;
 
 void *read_grades(void *filename) {
     FILE *file = fopen((char *) filename, "r");
@@ -64,28 +66,57 @@ void *read_grades(void *filename) {
     pthread_exit(0);
 }
 
+void *save_bellcurve(void *grade) {
+    pthread_mutex_lock(&file_lock);
+    FILE *file = fopen("bellcurve.txt", "a");
+    if (file == NULL) {
+        printf("Error writing on the file\n");
+        pthread_mutex_unlock(&file_lock);
+        pthread_exit(0);
+    }
+
+    pthread_mutex_lock(&lock);
+    int grade_value = *((int *)grade);
+    int curved_grade = grade_value * 1.5;
+    total_bellcurve_grade += curved_grade;
+    total_grade += grade_value;
+    fprintf(file, "%d\n", curved_grade);
+    pthread_mutex_unlock(&lock);
+
+    fclose(file);
+    pthread_mutex_unlock(&file_lock);
+    pthread_exit(0);
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Usage: %s <filename>\n", argv[0]);
         return 1;
     }
 
-    pthread_t thread;
+    pthread_t thread1, threads[10];
     pthread_mutex_init(&lock, NULL);
+    pthread_mutex_init(&file_lock, NULL);
     my_barrier_init(&barrier, 2);
 
-    pthread_create(&thread, NULL, read_grades, (void *)argv[1]);
-
+    pthread_create(&thread1, NULL, read_grades, (void *)argv[1]);
     my_barrier_wait(&barrier);
 
-    // Process grades after the barrier
     for (int i = 0; i < 10; i++) {
-        total_grade += grades[i];
+        pthread_create(&threads[i], NULL, save_bellcurve, &grades[i]);
     }
-    printf("Total grade: %d\n", total_grade);
 
-    pthread_join(thread, NULL);
+    for (int i = 0; i < 10; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("Total grade: %d\n", total_grade);
+    printf("Average before curve: %d\n", total_grade / 10);
+    printf("Average after curve: %d\n", total_bellcurve_grade / 10);
+
+    pthread_join(thread1, NULL);
     pthread_mutex_destroy(&lock);
+    pthread_mutex_destroy(&file_lock);
     my_barrier_destroy(&barrier);
 
     return 0;
